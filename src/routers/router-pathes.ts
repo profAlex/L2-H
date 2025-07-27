343dc0f04041e1a5333f8ad8306e93e2859e2b22
Alex <steblyakov.aleksandr@gmail.com>
2025-07-27 23:48:57 +0300

    Deploy version of the app.

--- src/routers/router-pathes.ts
+++ src/routers/router-pathes.ts
@@ -1,3 +1,3 @@
-export const TESTING_PATH = '/api/testing';
-export const POSTS_PATH = '/api/posts';
-export const BLOGS_PATH = '/api/blogs';
+export const TESTING_PATH = '/testing';
+export const POSTS_PATH = '/posts';
+export const BLOGS_PATH = '/blogs';
